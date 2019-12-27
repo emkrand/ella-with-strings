d3cb6db3382bd6f6a6df21460c4f831a10f0f117
emkrand <emkrand@users.noreply.github.com>
2019-12-27 01:46:44 -0600

    Added popup
    added a popup for lyrics, which is centered in the screen and appears when one of the songs is clicked. It can be closed with a 'close' button, and scrolls when the content is too large for the popup to handle.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var changeVisibility = function () {
+
+  var currentClassName = document.getElementById('popup').className;
+
+  if (currentClassName == "invisiblePopup") {
+    document.getElementById('popup').className = "visiblePopup";
+  } else {
+    document.getElementById('popup').className = "invisiblePopup";
+  }
+}
